1a2046c5789349d15656bc127b8b2ddcd07a28eb
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 01:06:49 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,55 @@
+
+        print(f"Random Number: {item}")
+    for item in data:
+
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    data = generate_random_data()
+if __name__ == "__main__":
+    main()
+def main():
+
+import random    for (let i = 0; i < 10; i++) {
+    });
+function main() {
+
+    data.forEach(item => {
+    }
+        console.log("Random Number: " + item);
+
+}
+
+    return data;
+}
+        data.push(Math.floor(Math.random() * 100) + 1);
+function generateRandomData() {
+    const data = generateRandomData();
+    const data = [];
+main();import random
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
+    return data
+    for item in data:
+def generate_random_data():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+
+
+if __name__ == "__main__":def generate_random_data():
+    data = generate_random_data()
+    return data
+    for item in data:
+
+    main()
+if __name__ == "__main__":
+
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+
+        print(f"Random Number: {item}")
+def main():
